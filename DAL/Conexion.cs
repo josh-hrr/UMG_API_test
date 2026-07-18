18f1abeae1f206a812dafa6c156d971320b10e70
joseodaniel <jperezs6@miumg.edu.gt>
2026-07-18 13:11:05 -0600

    Se crea la clase estática Conexion en DAL para centralizar la obtención de conexiones SQL Server usando la cadena definida en Web.config. Se registra Conexion.cs en UMG_API.csproj y se añade la sección connectionStrings con "UMGConnection".

---
+++ DAL/Conexion.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+using System.Configuration;
+
+namespace UMG_API.DAL
+{
+    public static class Conexion
+    {
+        private static readonly string connectionString =
+            ConfigurationManager.ConnectionStrings["UMGConnection"].ConnectionString;
+
+        /// <summary>
+        /// Devuelve una nueva instancia de conexión SQL, sin abrir.
+        /// El consumidor es responsable de abrirla/cerrarla (usar bloque using).
+        /// </summary>
+        public static SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
