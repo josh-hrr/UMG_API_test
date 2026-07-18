1f81514d4401d08a7236d9a26a86f17caccef0c7
danie <danie@JOSEPEREZ>
2026-07-18 12:37:04 -0600

    Agregar archivos de proyecto.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace UMG_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UMG_API.WebApiApplication" Language="C#" %>
